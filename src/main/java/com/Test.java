bf49963e6a6c4bb1ad4a92d24651a5badda82a74
Peter Ng <peter.ng@itiviti.com>
2021-12-04 00:27:55 +0800

    initial commit
    Change-Id: If689a793310eb419536de3c729c7842ef8d506fe

---
+++ src/main/java/com/Test.java
@@ -0,0 +1,12 @@
+package com;
+
+import jdk.jfr.events.SocketWriteEvent;
+
+public class Test
+{
+	public static void main() {
+		SocketWriteEvent event = new SocketWriteEvent();
+        event.begin();
+        event.commit();
+	}
+}
